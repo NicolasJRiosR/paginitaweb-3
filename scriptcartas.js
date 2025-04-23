91c16a43fcbbe9c7a194bb8178203b5442645588
nicolasStephany <nicolasjriosr@gmail.com>
2025-04-23 23:59:00 +0200

    Add files via upload

---
+++ scriptcartas.js
@@ -0,0 +1,22 @@
+const imagen = document.getElementById("cartaInteractiva");
+
+const imagenes = [
+  "1.png",
+  "2.png",
+  "3.png",
+  "4.png",
+  "5.png",
+  "6.png",
+  "7.png",
+  "8.png",
+  "9.png",
+  "10.png",
+  "11.png",
+];
+
+let indice = 0;
+
+imagen.addEventListener("click", () => {
+  indice = (indice + 1) % imagenes.length;
+  imagen.src = imagenes[indice];
+});
